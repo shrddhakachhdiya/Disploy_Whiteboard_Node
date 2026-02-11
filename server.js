d3774d8f185bd03bce74f955c50ab983db250d95
shrddhakachhdiya <shraddha.kachhdiya@thedestinysolutions.comshraddha.kachhdiya@thedestinysolutions.com>
2026-02-11 18:09:25 +0530

    change URL

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const server = http.createServer(app)
 //creating server instance for socket
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", "http://localhost:5174"],
+        origin: ["http://localhost:5173", "http://localhost:5174","https://disploy-whiteboard-react.vercel.app"],
         methods: ["GET", "POST"]
     }
 })
